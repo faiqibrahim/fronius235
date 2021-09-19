b5cd37a4aa1a64640737dfa7697ab6b06e3bb8c4
Ibraheem Faiq <ibraheem.faiq@synavos.com>
2021-09-19 12:00:30 +0500

    added client

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
 require('./db/pg');
 require('./redis/Redis');
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const basicAuth = require('express-basic-auth');
+const {authenticate} = require('./express/middlewares/index');
+
 const app = express();
 
 const {parseCurrentData, logCurrentData} = require('./utils/SolarUtils');
-
 const SolarService = require('./services/SolarService');
 const MeterService = require('./services/MeterService');
 
-app.use(basicAuth({
-    users: {
-        [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PW
-    }
-}));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(fileUpload({
-    limits: {fileSize: 50 * 1024 * 1024},
-}));
-
-app.post('/current-data', (req, resp) => {
-    resp.status(200).send({});
-
-    const data = parseCurrentData(req.body);
-    logCurrentData(data);
-
-    SolarService.saveStats(data).catch(console.error);
-});
 
 app.get('/current-power', (req, resp) => {
     SolarService.getCurrentOutput()
@@ -66,17 +49,6 @@ app.get('/year-stats', (req, resp) => {
         })
 });
 
-app.post('/meter-reading', (req, resp) => {
-    MeterService.saveMeterReading(req.body)
-        .then(reading => {
-            resp.status(200).send({reading});
-        })
-        .catch(err => {
-            console.error('Error occurred while saving meter-reading', err);
-            resp.status(500).send({});
-        })
-});
-
 app.get('/meter-readings', (req, resp) => {
     MeterService.getMeterReadings(req.query.lastReadingDate)
         .then(readings => {
@@ -99,7 +71,31 @@ app.get('/usage-stats', (req, resp) => {
         })
 });
 
-app.patch('/migrate', (req, resp) => {
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.post('/current-data', authenticate, (req, resp) => {
+    resp.status(200).send({});
+
+    const data = parseCurrentData(req.body);
+    logCurrentData(data);
+
+    SolarService.saveStats(data).catch(console.error);
+});
+
+app.post('/meter-reading', authenticate, (req, resp) => {
+    MeterService.saveMeterReading(req.body)
+        .then(reading => {
+            resp.status(200).send({reading});
+        })
+        .catch(err => {
+            console.error('Error occurred while saving meter-reading', err);
+            resp.status(500).send({});
+        })
+});
+
+app.patch('/migrate', authenticate, (req, resp) => {
     SolarService.loadMigrationData()
         .then(() => {
             resp.status(200).send({});
@@ -108,8 +104,7 @@ app.patch('/migrate', (req, resp) => {
             console.error('Error occurred while saving migration-data', err);
             resp.status(500).send({});
         })
-})
-
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- express/middlewares/index.js
+++ express/middlewares/index.js
@@ -1,3 +1,5 @@
+const basicAuth = require('express-basic-auth');
+
 const logRequestBody = (req, resp, next) => {
     console.info("\n########## REQ REC START ##########")
     console.info(req.originalUrl, new Date(), req.body);
@@ -6,6 +8,13 @@ const logRequestBody = (req, resp, next) => {
     next();
 }
 
+const authenticate = basicAuth({
+    users: {
+        [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PW
+    }
+});
+
 module.exports = {
-    logRequestBody
+    logRequestBody,
+    authenticate
 };
